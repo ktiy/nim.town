4e642e6202594a2b0faad99c8ec0ee7e6e184031
cricket <_c@piapiac.org>
2021-01-11 05:53:06 -0800

    remove redirects

--- dnsconfig.js
+++ dnsconfig.js
@@ -5,18 +5,13 @@ var CLOUDFLARE = NewDnsProvider("cloudflare", "CLOUDFLAREAPI", {"manage_redirect
 
 // alphabetical order please
 D("nim.town", REG_NONE, DnsProvider(CLOUDFLARE),
-    A("@", "207.246.105.83", CF_PROXY_ON),
-    AAAA("@", "2001:19f0:6001:1acf:5400:03ff:fe24:2896", CF_PROXY_ON),
+    A("@", "207.246.105.83"),
+    AAAA("@", "2001:19f0:6001:1acf:5400:03ff:fe24:2896"),
 
     CNAME("deser", "gabbhack.github.io."),
     CNAME("hayago", "liquidev.github.io."),
     CNAME("loco", "moigagoo.github.io."),
     CNAME("moigagoo", "moigagoo.github.io."),
     CNAME("norman", "moigagoo.github.io."),
-    CNAME("norm", "moigagoo.github.io."),
-
-    // except keep these at the bottom
-    CF_REDIRECT("packages.nim.town/*", "https://nimble.directory/$1"),
-    CF_REDIRECT("docs.nim.town/*", "https://nim-lang.org/documentation.html"),
-    CF_REDIRECT("doc.nim.town/*", "https://nim-lang.org/documentation.html")
+    CNAME("norm", "moigagoo.github.io.")
 )
