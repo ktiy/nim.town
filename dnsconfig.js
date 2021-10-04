b7ae0e772e79515573d22d5c6daabe4846e6cad2
c. c. piapiac <_c@piapiac.org>
2021-10-03 19:50:52 -0700

    new server for homepage
    though in the future i should probably just switch it to gh-pages :shrug:

--- dnsconfig.js
+++ dnsconfig.js
@@ -5,8 +5,8 @@ var CLOUDFLARE = NewDnsProvider("cloudflare", "CLOUDFLAREAPI");
 
 // alphabetical order please
 D("nim.town", REG_NONE, DnsProvider(CLOUDFLARE),
-    A("@", "207.246.105.83"),
-    AAAA("@", "2001:19f0:6001:1acf:5400:03ff:fe24:2896"),
+    A("@", "144.202.127.8"),
+    AAAA("@", "2001:19f0:6001:3d76:5400:03ff:fe99:fa79"),
 
     CNAME("deser", "gabbhack.github.io."),
     CNAME("gemi", "prunariu.space-nomad.com."),
