a826f8c261ff713398bad67d0fe02482c12aad39
ourstorycomic <100331812+ourstorycomic@users.noreply.github.com>
2022-08-22 13:14:37 +0700

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -15,5 +15,6 @@ D("nim.town", REG_NONE, DnsProvider(CLOUDFLARE),
     CNAME("moigagoo", "moigagoo.github.io."),
     CNAME("norman", "moigagoo.github.io."),
     CNAME("norm", "moigagoo.github.io."),
+    CNAME("rwstudio", "ourstorycomic.github.io."),
     CNAME("xidoc", "pages.sr.ht.")
 );
