f571e8cf732c768a4c0383b6cf07ebd1bcc84893
ktiy <cricket@piapiac.org>
2024-03-25 02:28:52 +0000

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -5,9 +5,6 @@ var CLOUDFLARE = NewDnsProvider("cloudflare", "CLOUDFLAREAPI");
 
 // alphabetical order please
 D("nim.town", REG_NONE, DnsProvider(CLOUDFLARE),
-    A("@", "144.202.127.8"),
-    AAAA("@", "2001:19f0:6001:3d76:5400:03ff:fe99:fa79"),
-
     CNAME("deser", "gabbhack.github.io."),
     CNAME("gemi", "prunariu.space-nomad.com."),
     CNAME("hayago", "liquidev.github.io."),
@@ -19,5 +16,14 @@ D("nim.town", REG_NONE, DnsProvider(CLOUDFLARE),
     CNAME("nova", "neroist.github.io."),
     CNAME("rwstudio", "ourstorycomic.github.io."),
     CNAME("uing", "neroist.github.io."),
-    CNAME("xidoc", "pages.sr.ht.")
+    CNAME("xidoc", "pages.sr.ht."),
+
+    A("@", "185.199.108.153"),
+    A("@", "185.199.109.153"),
+    A("@", "185.199.110.153"),
+    A("@", "185.199.111.153"),
+    AAAA("@", "2606:50c0:8000::153"),
+    AAAA("@", "2606:50c0:8001::153"),
+    AAAA("@", "2606:50c0:8002::153"),
+    AAAA("@", "2606:50c0:8003::153")
 );
